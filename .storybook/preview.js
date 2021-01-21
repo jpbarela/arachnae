c1a099e9ae58ada3983256a2f036448705ad90aa
JP Barela <barela@jpbarela.com>
2021-01-20 21:43:11 -0700

    Configure Storybook to use stories directory

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import React from "react";
+import { defaultTheme } from "../src/Theme";
+import { ThemeProvider } from "react-jss";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={defaultTheme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
